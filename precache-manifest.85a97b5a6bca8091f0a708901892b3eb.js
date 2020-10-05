4348e2b9a3dce3058cadf3ebbfb4e1e055a4df63
Nam Dang <sote1412@gmail.com>
2020-10-05 14:25:45 +0700

    Updates

---
+++ precache-manifest.85a97b5a6bca8091f0a708901892b3eb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b24bea64a737a2bbb4461a6cc2e735",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "9057255caf933ffbfae7",
+    "url": "/reactjs/static/css/2.42caf7ee.chunk.css"
+  },
+  {
+    "revision": "f91b0c9df996d45f6491",
+    "url": "/reactjs/static/css/main.0bde720a.chunk.css"
+  },
+  {
+    "revision": "9057255caf933ffbfae7",
+    "url": "/reactjs/static/js/2.ae13fe78.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/reactjs/static/js/2.ae13fe78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f91b0c9df996d45f6491",
+    "url": "/reactjs/static/js/main.2058d00c.chunk.js"
+  },
+  {
+    "revision": "e19ebca9e674120ac6e0",
+    "url": "/reactjs/static/js/runtime-main.2602d810.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/reactjs/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/reactjs/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/reactjs/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/reactjs/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/reactjs/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
